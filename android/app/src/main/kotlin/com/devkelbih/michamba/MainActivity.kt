2618d8957fd811a9e8e2acc8b385014d049ba81b
Henry Ramirez <kelbicanchari@gmail.com>
2026-04-21 02:09:41 -0500

    puliendo la app arranque

--- android/app/src/main/kotlin/com/devkelbih/michamba/MainActivity.kt
+++ android/app/src/main/kotlin/com/devkelbih/michamba/MainActivity.kt
@@ -1,20 +1,6 @@
 package com.devkelbih.michamba
 
-import android.content.Intent
-import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-    if (savedInstanceState != null) {
-        val intent = Intent(applicationContext, MainActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-        startActivity(intent)
-        finish()
-        return // evita que Flutter inicialice dos veces
-    }
-
-    super.onCreate(null) // forzamos inicio limpio del engine
-}
-
+class MainActivity: FlutterActivity() {
 }
