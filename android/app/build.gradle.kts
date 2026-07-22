1034a3f667bfd46dea648dbb2471c1950b53bbd0
Henry Ramirez <kelbicanchari@gmail.com>
2026-07-21 20:24:59 -0500

    Actualizacion de target, nueva version 0.1.3+5

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
     defaultConfig {
         applicationId = "com.devkelbih.michamba"
         minSdk = 28
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode?.toInt() ?: 1
         versionName = flutter.versionName ?: "1.0.0"
     }
